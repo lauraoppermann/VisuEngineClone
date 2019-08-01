4b0ca75070d3834e09cb3619f97c3c38e7885654
flo <florian.teich@campus.tu-berlin.de>
2019-08-01 12:08:59 +0200

    Version 1.0 | Build script included | Further documentation coming soon

--- src/index.js
+++ src/index.js
@@ -4,13 +4,11 @@ const Renderer = require("./renderer/Renderer")
 const PluginManager = require("./pluginmanager/PluginManager")
 const CONSTANTS = require("./Constants")
 
-
 // create instances
 const database = new Database(CONSTANTS.DB_PATH)
 const renderer = new Renderer()
 const pluginManager = new PluginManager()
 const server = new Server(CONSTANTS.SERVER_PORT, database, renderer, pluginManager)
-
 
 // launch server
 server.start()
