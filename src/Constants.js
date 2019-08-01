7698c6f4dee58d07ce808a63b851058a7474aa16
flo <florian.teich@campus.tu-berlin.de>
2019-08-01 11:31:47 +0200

    added DB and Server constants | use new constants now

--- src/Constants.js
+++ src/Constants.js
@@ -1,10 +1,15 @@
 const path = require("path")
 const process = require("process")
 
+// runtime constants
+const SERVER_PORT = 8000
+const DB_NAME = "ChartDatabase.json"
+
 // path constants
 const APP_ROOT = path.dirname(process.argv[1])
 const UNIT_PATH = path.join(APP_ROOT, "renderer/units/")
 const PLUGIN_PATH = path.join(APP_ROOT, "pluginmanager/plugins")
+const DB_PATH = path.join(APP_ROOT, DB_NAME)
 
 // plugin manager actions
 const BEFORE_DB_ACTION = "before_database"
@@ -19,9 +24,12 @@ console.log(UNIT_PATH)
 console.log(PLUGIN_PATH)
 
 module.exports = {
+    SERVER_PORT : SERVER_PORT,
+    DB_NAME : DB_NAME,
     APP_ROOT : APP_ROOT,
     UNIT_PATH : UNIT_PATH,
     PLUGIN_PATH : PLUGIN_PATH,
+    DB_PATH : DB_PATH,
     BEFORE_DB_ACTION : BEFORE_DB_ACTION,
     AFTER_DB_ACTION : AFTER_DB_ACTION,
     BEFORE_RENDERER_ACTION : BEFORE_RENDERER_ACTION,
--- src/index.js
+++ src/index.js
@@ -2,15 +2,14 @@ const Database = require("./database/Database")
 const Server = require("./api/Server")
 const Renderer = require("./renderer/Renderer")
 const PluginManager = require("./pluginmanager/PluginManager")
+const CONSTANTS = require("./Constants")
 
-const SERVER_PORT = 8000
-const DB_NAME = "ChartDatabase.json"
 
 // create instances
-const database = new Database(DB_NAME)
+const database = new Database(CONSTANTS.DB_PATH)
 const renderer = new Renderer()
 const pluginManager = new PluginManager()
-const server = new Server(SERVER_PORT, database, renderer, pluginManager)
+const server = new Server(CONSTANTS.SERVER_PORT, database, renderer, pluginManager)
 
 
 // launch server
