07508484f223a00680e05bab1c2330716154e54c
Laura Oppermann <laura.oppermann98@gmx.de>
2019-09-19 16:10:04 +0200

    added livestreaming

--- src/renderer/Renderer.js
+++ src/renderer/Renderer.js
@@ -5,43 +5,42 @@ const path = require("path")
 const CONSTANTS = require("../Constants")
 
 class Renderer {
-    
+
     constructor() {
         this.units = this.loadUnits()
     }
-    
+
     loadUnits() {
-        
+
         // find all units
         let result = {}
         let units = fs.readdirSync(CONSTANTS.UNIT_PATH)
-        
+
         // iterate over units and parse information
         units.forEach((unit) => {
             let unitPath = path.join(CONSTANTS.UNIT_PATH, unit, "/unit.js")
             console.log("Loaded Unit:", unit)
             result[unit] = require(unitPath)
         })
-        
+
         return result
     }
-    
+
     unitExists(unit) {
         return Object.keys(this.units).includes(unit)
     }
-    
+
     render(unit, template, chartJson) {
-        
         if (!this.unitExists(unit)) {
             return false
         }
-        
+
         let unitHomeDir = path.join(CONSTANTS.UNIT_PATH, unit)
-        
+
         // run unit and return result
         return this.units[unit](chartJson, template, unitHomeDir)
     }
-    
+
 }
 
 module.exports = Renderer
